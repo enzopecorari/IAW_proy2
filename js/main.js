6e9883eff50e32a3f6190b76f7c9dc7978591ab5
Enzo <enzo_pecorari@hotmail.com>
2012-04-26 22:38:42 -0300

    cambio de estilo implementado y comienzo estilo 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-$(document).ready(function() { 
 
+$(document).ready(function() { 
+	
 	cargarPromos();
 	$('#nextPromo').click(function() {
 		cargarPromos();
@@ -115,4 +116,17 @@ function actualizarPedido() {
 		"<span class='pesoProducto'>"+prod.getPeso()*cant+"gr. </span>" +	"</div></li>"; 
 	}
 	$('#listaPedido').html(lista);
+}
+
+//CAMBIAR CSS
+function cambiarHojaDeEstilos(title) {
+    var i, a, main;
+    for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    	if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")){
+            a.disabled = true;
+            if(a.getAttribute("title") == title) a.disabled = false;
+            if(a.getAttribute("title") == "main") a.disabled = false;
+            if(a.getAttribute("title") == "mobile") a.disabled = false;
+        }
+    }
 }
